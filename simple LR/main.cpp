f4b8cedea3d3b1c3c1e576077688b0c039653b71
aaditya19saini <asaini_btech23@thapar.edu>
2026-09-19 01:13:39 +0530

    Covariance

--- simple LR/main.cpp
+++ simple LR/main.cpp
@@ -3,27 +3,31 @@
 #include<cmath>
 using namespace std;
 
-double Mean(vector<double>value){
+double getMean(vector<double>value){
     double sum=0;
     for (auto value:value){
         sum+=value; }
 return sum/value.size();}
 
 double getvariance(vector<double>value){
-    double mean=getmean(value);
+    double mean=getMean(value);
     double variance=0;
-    for (auto value:values){
+    for (auto value:value){
         variance+=pow(value-mean,2);
     }
     return variance;
 
 }
 double getcovarinace(vector<double>valueA,vector<double>valueB){
-    double meanA=getmean(valueA);
-    double meanB=getmean(valueB);
+    double meanA=getMean(valueA);
+    double meanB=getMean(valueB);
     double covariance=0;
-    for (auto value:values){
-
+    for (int i=0;i<valueA.size();i++){
+        covariance+=(valueA[i]-meanA)*(valueB[i]-meanB);
     }
+    return covariance;
 
+}
+int main(){
+    return 0;
 }
